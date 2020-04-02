ac552a16f344938a99d2cf1630ea369ea0c5b582
cyX_GUG <chenyangcyx@outlook.com>
2020-04-03 03:02:01 +0800

    创建了NET Framwork48项目 时间： 2020年4月3日03:01:52

---
+++ Assessment Tool Prototype/NET Framwork48/Models/ModelData.cs
@@ -0,0 +1,14 @@
+﻿using NET_Framwork48.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET_Framwork48.Models
+{
+    class ModelData
+    {
+        
+    }
+}
---
+++ Assessment Tool Prototype/NET Framwork48/Utils/OverAllData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET_Framwork48.Utils
+{
+    class OverAllData
+    {
+        public static OverAllData allData = new OverAllData();
+
+        //是否评估 Modularity
+        public bool if_assess_modularity = true;
+    }
+}
