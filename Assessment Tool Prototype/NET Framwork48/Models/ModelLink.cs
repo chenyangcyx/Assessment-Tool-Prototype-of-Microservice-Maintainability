50935d7201eb875b0ccb64dafccd7d525e58a399
cyX_GUG <chenyangcyx@outlook.com>
2020-04-04 02:02:45 +0800

    重写了ModelLink的设计方法 时间： 2020年4月4日02:02:43

--- Assessment Tool Prototype/NET Framwork48/Models/ModelLink.cs
+++ Assessment Tool Prototype/NET Framwork48/Models/ModelLink.cs
@@ -8,33 +8,149 @@ namespace NET_Framwork48.Models
 {
     public class ModelLink
     {
-        public const int LEVEL1_UNITNUM = 1;           //层1的数量
-        public const int LEVEL2_UNITNUM = 4;           //层2的数量
-        public const int LEVEL3_UNITNUM = 7;           //层3的数量
-        public const int LEVEL4_UNITNUM = 8;           //层4的数量
+        public const int LEVEL1_UNITNUM = 1;           //层1的元素数量（不重复）
+        public const int LEVEL2_UNITNUM = 4;           //层2的元素数量（不重复）
+        public const int LEVEL3_UNITNUM = 7;           //层3的元素数量（不重复）
+        public const int LEVEL4_UNITNUM = 8;           //层4的元素数量（不重复）
 
-        public const int LEVEL1_1_LINK_2_NUM = 4;       //层1-1连接到层2的元素个数
-        public const int LEVEL2_1_LINK_3_NUM = 4;       //层2-1连接到层3的元素个数
-        public const int LEVEL2_2_LINK_3_NUM = 5;       //层2-2连接到层3的元素个数
-        public const int LEVEL2_3_LINK_3_NUM = 2;       //层2-3连接到层3的元素个数
-        public const int LEVEL2_4_LINK_3_NUM = 2;       //层2-4连接到层3的元素个数
-        public const int LEVEL3_1_LINK_4_NUM = 2;       //层3-1连接到层4的元素个数
-        public const int LEVEL3_2_LINK_4_NUM = 2;       //层3-2连接到层4的元素个数
-        public const int LEVEL3_3_LINK_4_NUM = 2;       //层3-3连接到层4的元素个数
-        public const int LEVEL3_4_LINK_4_NUM = 3;       //层3-4连接到层4的元素个数
-        public const int LEVEL3_5_LINK_4_NUM = 2;       //层3-5连接到层4的元素个数
-        public const int LEVEL3_6_LINK_4_NUM = 2;       //层3-6连接到层4的元素个数
-        public const int LEVEL3_7_LINK_4_NUM = 3;       //层3-7连接到层4的元素个数
-        public const int LEVEL3_8_LINK_4_NUM = 3;       //层3-8连接到层4的元素个数
-        public const int LEVEL3_9_LINK_4_NUM = 2;       //层3-9连接到层4的元素个数
-        public const int LEVEL3_10_LINK_4_NUM = 3;      //层3-10连接到层4的元素个数
-        public const int LEVEL3_11_LINK_4_NUM = 2;      //层3-11连接到层4的元素个数
-        public const int LEVEL3_12_LINK_4_NUM = 2;      //层3-12连接到层4的元素个数
-        public const int LEVEL3_13_LINK_4_NUM = 3;      //层3-13连接到层4的元素个数
+        //层1连接到层2的元素个数
+        public const int LEVEL1_LINK_2_NUM = 4;
+        //层2连接到层3的元素个数
+        public int[] LEVEL2_1_LINK_3_NUM = new int[4]{ 4, 5, 2, 2 };
+        //层3连接到层4的元素个数
+        public int[][] LEVEL3_1_LINK_4_NUM = { 
+            new int[4]{ 2, 2, 2, 3 }, 
+            new int[5]{ 2, 2, 3, 3, 2 },
+            new int[2]{ 3, 2 },
+            new int[2]{ 2, 3 } };
 
-        public const int LEVEL1_TOTAL_NUM = 1;          //层1的所有节点个数
-        public const int LEVEL2_TOTAL_NUM = 4;          //层1的所有节点个数
-        public const int LEVEL3_TOTAL_NUM = 13;         //层1的所有节点个数
-        public const int LEVEL4_TOTAL_NUM = 31;         //层1的所有节点个数
+        public const int LEVEL1_TOTAL_NUM = 1;          //层1的节点个数（重复）
+        public const int LEVEL2_TOTAL_NUM = 4;          //层2的节点个数（重复）
+        public const int LEVEL3_TOTAL_NUM = 13;         //层3的节点个数（重复）
+        public const int LEVEL4_TOTAL_NUM = 31;         //层4的节点个数（重复）
+
+        //层1下面涉及的第2层元素名称列表
+        List<string> LEVEL1_LINK_2_UNITS = new List<string>()
+        {
+            ModelName.LEVELNAME_ATTRIBUTE_1,
+            ModelName.LEVELNAME_ATTRIBUTE_2,
+            ModelName.LEVELNAME_ATTRIBUTE_3,
+            ModelName.LEVELNAME_ATTRIBUTE_4
+        };
+        //层2下面涉及的第3层元素名称列表
+        List<List<string>> LEVEL2_LINK_3_UNITS = new List<List<string>>()
+        {
+            new List<string>()
+            {
+                ModelName.LEVELNAME_PROPERTY_1,
+                ModelName.LEVELNAME_PROPERTY_2,
+                ModelName.LEVELNAME_PROPERTY_3,
+                ModelName.LEVELNAME_PROPERTY_4
+            },
+            new List<string>()
+            {
+                ModelName.LEVELNAME_PROPERTY_1,
+                ModelName.LEVELNAME_PROPERTY_2,
+                ModelName.LEVELNAME_PROPERTY_5,
+                ModelName.LEVELNAME_PROPERTY_6,
+                ModelName.LEVELNAME_PROPERTY_7
+            },
+            new List<string>()
+            {
+                ModelName.LEVELNAME_PROPERTY_6,
+                ModelName.LEVELNAME_PROPERTY_7
+            },
+            new List<string>()
+            {
+                ModelName.LEVELNAME_PROPERTY_2,
+                ModelName.LEVELNAME_PROPERTY_5
+            }
+        };
+        //层3下面涉及的第4层元素名称列表
+        List<List<List<string>>> LEVEL3_LINK_4_UNITS = new List<List<List<string>>>()
+        {
+            new List<List<string>>()
+            {
+                new List<string>()
+                {
+                    ModelName.LEVELNAME_METRIC_1,
+                    ModelName.LEVELNAME_METRIC_2
+                },
+                new List<string>()
+                {
+                    ModelName.LEVELNAME_METRIC_1,
+                    ModelName.LEVELNAME_METRIC_3
+                },
+                new List<string>()
+                {
+                    ModelName.LEVELNAME_METRIC_1,
+                    ModelName.LEVELNAME_METRIC_4
+                },
+                new List<string>()
+                {
+                    ModelName.LEVELNAME_METRIC_1,
+                    ModelName.LEVELNAME_METRIC_5,
+                    ModelName.LEVELNAME_METRIC_6
+                }
+            },
+            new List<List<string>>()
+            {
+                new List<string>()
+                {
+                    ModelName.LEVELNAME_METRIC_1,
+                    ModelName.LEVELNAME_METRIC_2
+                },
+                new List<string>()
+                {
+                    ModelName.LEVELNAME_METRIC_1,
+                    ModelName.LEVELNAME_METRIC_3
+                },
+                new List<string>()
+                {
+                    ModelName.LEVELNAME_METRIC_1,
+                    ModelName.LEVELNAME_METRIC_3,
+                    ModelName.LEVELNAME_METRIC_5
+                },
+                new List<string>()
+                {
+                    ModelName.LEVELNAME_METRIC_3,
+                    ModelName.LEVELNAME_METRIC_7,
+                    ModelName.LEVELNAME_METRIC_5
+                },
+                new List<string>()
+                {
+                    ModelName.LEVELNAME_METRIC_1,
+                    ModelName.LEVELNAME_METRIC_8
+                }
+            },
+            new List<List<string>>()
+            {
+                new List<string>()
+                {
+                    ModelName.LEVELNAME_METRIC_3,
+                    ModelName.LEVELNAME_METRIC_7,
+                    ModelName.LEVELNAME_METRIC_5
+                },
+                new List<string>()
+                {
+                    ModelName.LEVELNAME_METRIC_1,
+                    ModelName.LEVELNAME_METRIC_8
+                }
+            },
+            new List<List<string>>()
+            {
+                new List<string>()
+                {
+                    ModelName.LEVELNAME_METRIC_1,
+                    ModelName.LEVELNAME_METRIC_3
+                },
+                new List<string>()
+                {
+                    ModelName.LEVELNAME_METRIC_1,
+                    ModelName.LEVELNAME_METRIC_3,
+                    ModelName.LEVELNAME_METRIC_5
+                }
+            }
+        };
     }
 }
