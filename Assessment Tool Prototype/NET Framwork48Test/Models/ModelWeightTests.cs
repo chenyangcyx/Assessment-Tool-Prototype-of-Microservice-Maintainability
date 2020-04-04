247a4d2a247c9bf0b842cbd5cfef182245943c8f
cyX_GUG <chenyangcyx@outlook.com>
2020-04-05 02:41:38 +0800

    为SetNodeGainORDamage函数创建了单元测试 时间： 2020年4月5日02:41:37

--- Assessment Tool Prototype/NET Framwork48Test/Models/ModelWeightTests.cs
+++ Assessment Tool Prototype/NET Framwork48Test/Models/ModelWeightTests.cs
@@ -78,5 +78,81 @@ namespace NET_Framwork48.Models.Tests
             mw.node_affect_dictionary.TryGetValue(ModelName.LEVELNAME_METRIC_8, out result);
             Assert.AreEqual(ModelWeight.NODE_AFFECT_GAIN, result);
         }
+
+        [TestMethod()]
+        public void SetNodeGainORDamageTest()
+        {
+            List<ModelNode> level1_nodes = new List<ModelNode>();
+            List<ModelNode> level2_nodes = new List<ModelNode>();
+            List<ModelNode> level3_nodes = new List<ModelNode>();
+            List<ModelNode> level4_nodes = new List<ModelNode>();
+            ModelName mn = new ModelName();
+            ModelWeight mw = new ModelWeight();
+
+            ModelNode[] l1n = new ModelNode[ModelLink.LEVEL1_UNITNUM];
+            ModelNode[] l2n = new ModelNode[ModelLink.LEVEL2_UNITNUM];
+            ModelNode[] l3n = new ModelNode[ModelLink.LEVEL3_UNITNUM];
+            ModelNode[] l4n = new ModelNode[ModelLink.LEVEL4_UNITNUM];
+            for (int i = 0; i < ModelLink.LEVEL1_UNITNUM; i++)
+                l1n[i] = new ModelNode();
+            for (int i = 0; i < ModelLink.LEVEL2_UNITNUM; i++)
+                l2n[i] = new ModelNode();
+            for (int i = 0; i < ModelLink.LEVEL3_UNITNUM; i++)
+                l3n[i] = new ModelNode();
+            for (int i = 0; i < ModelLink.LEVEL4_UNITNUM; i++)
+                l4n[i] = new ModelNode();
+
+            mn.SetNodeName(l1n[0], ModelName.LEVELNAME_QUALITYATTRIBUTE_1);
+            mn.SetNodeName(l2n[0], ModelName.LEVELNAME_ATTRIBUTE_1);
+            mn.SetNodeName(l2n[1], ModelName.LEVELNAME_ATTRIBUTE_2);
+            mn.SetNodeName(l2n[2], ModelName.LEVELNAME_ATTRIBUTE_3);
+            mn.SetNodeName(l2n[3], ModelName.LEVELNAME_ATTRIBUTE_4);
+            mn.SetNodeName(l3n[0], ModelName.LEVELNAME_PROPERTY_1);
+            mn.SetNodeName(l3n[1], ModelName.LEVELNAME_PROPERTY_2);
+            mn.SetNodeName(l3n[2], ModelName.LEVELNAME_PROPERTY_3);
+            mn.SetNodeName(l3n[3], ModelName.LEVELNAME_PROPERTY_4);
+            mn.SetNodeName(l3n[4], ModelName.LEVELNAME_PROPERTY_5);
+            mn.SetNodeName(l3n[5], ModelName.LEVELNAME_PROPERTY_6);
+            mn.SetNodeName(l3n[6], ModelName.LEVELNAME_PROPERTY_7);
+            mn.SetNodeName(l4n[0], ModelName.LEVELNAME_METRIC_1);
+            mn.SetNodeName(l4n[1], ModelName.LEVELNAME_METRIC_2);
+            mn.SetNodeName(l4n[2], ModelName.LEVELNAME_METRIC_3);
+            mn.SetNodeName(l4n[3], ModelName.LEVELNAME_METRIC_4);
+            mn.SetNodeName(l4n[4], ModelName.LEVELNAME_METRIC_5);
+            mn.SetNodeName(l4n[5], ModelName.LEVELNAME_METRIC_6);
+            mn.SetNodeName(l4n[6], ModelName.LEVELNAME_METRIC_7);
+            mn.SetNodeName(l4n[7], ModelName.LEVELNAME_METRIC_8);
+
+            for (int i = 0; i < ModelLink.LEVEL1_UNITNUM; i++)
+                mw.SetNodeGainORDamage(l1n[i]);
+            for (int i = 0; i < ModelLink.LEVEL2_UNITNUM; i++)
+                mw.SetNodeGainORDamage(l2n[i]);
+            for (int i = 0; i < ModelLink.LEVEL3_UNITNUM; i++)
+                mw.SetNodeGainORDamage(l3n[i]);
+            for (int i = 0; i < ModelLink.LEVEL4_UNITNUM; i++)
+                mw.SetNodeGainORDamage(l4n[i]);
+
+            int result;
+            for (int i = 0; i < ModelLink.LEVEL1_UNITNUM; i++)
+            {
+                mw.node_affect_dictionary.TryGetValue(l1n[i].NodeName, out result);
+                Assert.AreEqual(result, l1n[i].gain_or_damage);
+            }
+            for (int i = 0; i < ModelLink.LEVEL2_UNITNUM; i++)
+            {
+                mw.node_affect_dictionary.TryGetValue(l2n[i].NodeName, out result);
+                Assert.AreEqual(result, l2n[i].gain_or_damage);
+            }
+            for (int i = 0; i < ModelLink.LEVEL3_UNITNUM; i++)
+            {
+                mw.node_affect_dictionary.TryGetValue(l3n[i].NodeName, out result);
+                Assert.AreEqual(result, l3n[i].gain_or_damage);
+            }
+            for (int i = 0; i < ModelLink.LEVEL4_UNITNUM; i++)
+            {
+                mw.node_affect_dictionary.TryGetValue(l4n[i].NodeName, out result);
+                Assert.AreEqual(result, l4n[i].gain_or_damage);
+            }
+        }
     }
 }
