3a0776f075ba3b267bb88fdd376793489e8945b2
cyX_GUG <chenyangcyx@outlook.com>
2020-04-05 02:22:02 +0800

    添加了CreateModelNodeAffectDictionary函数的测试用例 时间： 2020年4月5日02:22:01

--- Assessment Tool Prototype/NET Framwork48Test/Models/ModelWeightTests.cs
+++ Assessment Tool Prototype/NET Framwork48Test/Models/ModelWeightTests.cs
@@ -11,5 +11,72 @@ namespace NET_Framwork48.Models.Tests
     [TestClass()]
     public class ModelWeightTests
     {
+        [TestMethod()]
+        public void CreateModelNodeAffectDictionaryTest()
+        {
+            ModelWeight mw = new ModelWeight();
+            Assert.IsTrue(mw.node_affect_dictionary.TryGetValue(ModelName.LEVELNAME_QUALITYATTRIBUTE_1, out _));
+            Assert.IsTrue(mw.node_affect_dictionary.TryGetValue(ModelName.LEVELNAME_ATTRIBUTE_1, out _));
+            Assert.IsTrue(mw.node_affect_dictionary.TryGetValue(ModelName.LEVELNAME_ATTRIBUTE_2, out _));
+            Assert.IsTrue(mw.node_affect_dictionary.TryGetValue(ModelName.LEVELNAME_ATTRIBUTE_3, out _));
+            Assert.IsTrue(mw.node_affect_dictionary.TryGetValue(ModelName.LEVELNAME_ATTRIBUTE_4, out _));
+            Assert.IsTrue(mw.node_affect_dictionary.TryGetValue(ModelName.LEVELNAME_PROPERTY_1, out _));
+            Assert.IsTrue(mw.node_affect_dictionary.TryGetValue(ModelName.LEVELNAME_PROPERTY_2, out _));
+            Assert.IsTrue(mw.node_affect_dictionary.TryGetValue(ModelName.LEVELNAME_PROPERTY_3, out _));
+            Assert.IsTrue(mw.node_affect_dictionary.TryGetValue(ModelName.LEVELNAME_PROPERTY_4, out _));
+            Assert.IsTrue(mw.node_affect_dictionary.TryGetValue(ModelName.LEVELNAME_PROPERTY_5, out _));
+            Assert.IsTrue(mw.node_affect_dictionary.TryGetValue(ModelName.LEVELNAME_PROPERTY_6, out _));
+            Assert.IsTrue(mw.node_affect_dictionary.TryGetValue(ModelName.LEVELNAME_PROPERTY_7, out _));
+            Assert.IsTrue(mw.node_affect_dictionary.TryGetValue(ModelName.LEVELNAME_METRIC_1, out _));
+            Assert.IsTrue(mw.node_affect_dictionary.TryGetValue(ModelName.LEVELNAME_METRIC_2, out _));
+            Assert.IsTrue(mw.node_affect_dictionary.TryGetValue(ModelName.LEVELNAME_METRIC_3, out _));
+            Assert.IsTrue(mw.node_affect_dictionary.TryGetValue(ModelName.LEVELNAME_METRIC_4, out _));
+            Assert.IsTrue(mw.node_affect_dictionary.TryGetValue(ModelName.LEVELNAME_METRIC_5, out _));
+            Assert.IsTrue(mw.node_affect_dictionary.TryGetValue(ModelName.LEVELNAME_METRIC_6, out _));
+            Assert.IsTrue(mw.node_affect_dictionary.TryGetValue(ModelName.LEVELNAME_METRIC_7, out _));
+            Assert.IsTrue(mw.node_affect_dictionary.TryGetValue(ModelName.LEVELNAME_METRIC_8, out _));
+
+            int result;
+            mw.node_affect_dictionary.TryGetValue(ModelName.LEVELNAME_QUALITYATTRIBUTE_1, out result);
+            Assert.AreEqual(ModelWeight.NODE_AFFECT_GAIN, result);
+            mw.node_affect_dictionary.TryGetValue(ModelName.LEVELNAME_ATTRIBUTE_1, out result);
+            Assert.AreEqual(ModelWeight.NODE_AFFECT_GAIN, result);
+            mw.node_affect_dictionary.TryGetValue(ModelName.LEVELNAME_ATTRIBUTE_2, out result);
+            Assert.AreEqual(ModelWeight.NODE_AFFECT_GAIN, result);
+            mw.node_affect_dictionary.TryGetValue(ModelName.LEVELNAME_ATTRIBUTE_3, out result);
+            Assert.AreEqual(ModelWeight.NODE_AFFECT_GAIN, result);
+            mw.node_affect_dictionary.TryGetValue(ModelName.LEVELNAME_ATTRIBUTE_4, out result);
+            Assert.AreEqual(ModelWeight.NODE_AFFECT_GAIN, result);
+            mw.node_affect_dictionary.TryGetValue(ModelName.LEVELNAME_PROPERTY_1, out result);
+            Assert.AreEqual(ModelWeight.NODE_AFFECT_DAMAGE, result);
+            mw.node_affect_dictionary.TryGetValue(ModelName.LEVELNAME_PROPERTY_2, out result);
+            Assert.AreEqual(ModelWeight.NODE_AFFECT_GAIN, result);
+            mw.node_affect_dictionary.TryGetValue(ModelName.LEVELNAME_PROPERTY_3, out result);
+            Assert.AreEqual(ModelWeight.NODE_AFFECT_DAMAGE, result);
+            mw.node_affect_dictionary.TryGetValue(ModelName.LEVELNAME_PROPERTY_4, out result);
+            Assert.AreEqual(ModelWeight.NODE_AFFECT_DAMAGE, result);
+            mw.node_affect_dictionary.TryGetValue(ModelName.LEVELNAME_PROPERTY_5, out result);
+            Assert.AreEqual(ModelWeight.NODE_AFFECT_GAIN, result);
+            mw.node_affect_dictionary.TryGetValue(ModelName.LEVELNAME_PROPERTY_6, out result);
+            Assert.AreEqual(ModelWeight.NODE_AFFECT_GAIN, result);
+            mw.node_affect_dictionary.TryGetValue(ModelName.LEVELNAME_PROPERTY_7, out result);
+            Assert.AreEqual(ModelWeight.NODE_AFFECT_DAMAGE, result);
+            mw.node_affect_dictionary.TryGetValue(ModelName.LEVELNAME_METRIC_1, out result);
+            Assert.AreEqual(ModelWeight.NODE_AFFECT_GAIN, result);
+            mw.node_affect_dictionary.TryGetValue(ModelName.LEVELNAME_METRIC_2, out result);
+            Assert.AreEqual(ModelWeight.NODE_AFFECT_GAIN, result);
+            mw.node_affect_dictionary.TryGetValue(ModelName.LEVELNAME_METRIC_3, out result);
+            Assert.AreEqual(ModelWeight.NODE_AFFECT_GAIN, result);
+            mw.node_affect_dictionary.TryGetValue(ModelName.LEVELNAME_METRIC_4, out result);
+            Assert.AreEqual(ModelWeight.NODE_AFFECT_GAIN, result);
+            mw.node_affect_dictionary.TryGetValue(ModelName.LEVELNAME_METRIC_5, out result);
+            Assert.AreEqual(ModelWeight.NODE_AFFECT_GAIN, result);
+            mw.node_affect_dictionary.TryGetValue(ModelName.LEVELNAME_METRIC_6, out result);
+            Assert.AreEqual(ModelWeight.NODE_AFFECT_GAIN, result);
+            mw.node_affect_dictionary.TryGetValue(ModelName.LEVELNAME_METRIC_7, out result);
+            Assert.AreEqual(ModelWeight.NODE_AFFECT_GAIN, result);
+            mw.node_affect_dictionary.TryGetValue(ModelName.LEVELNAME_METRIC_8, out result);
+            Assert.AreEqual(ModelWeight.NODE_AFFECT_GAIN, result);
+        }
     }
 }
