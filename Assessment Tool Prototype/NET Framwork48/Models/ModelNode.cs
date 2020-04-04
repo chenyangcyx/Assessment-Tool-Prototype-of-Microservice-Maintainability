829381688b045357ad9d64d2ea00d7819e4674ee
cyX_GUG <chenyangcyx@outlook.com>
2020-04-04 17:57:43 +0800

    构建了ModelWeight模块 时间： 2020年4月4日17:57:42

--- Assessment Tool Prototype/NET Framwork48/Models/ModelNode.cs
+++ Assessment Tool Prototype/NET Framwork48/Models/ModelNode.cs
@@ -11,8 +11,9 @@ namespace NET_Framwork48.Models
         public string NodeLevelName { get; set; }                       //节点层级名称
         public List<ModelNode> nextlevel;                               //下一层的节点引用
         public ModelNode prelevel;                                      //上一层的相连节点
-        public Decimal weight { get; set; }                             //权值
-        public Decimal value { get; set; }                              //计算数值
+        public decimal weight { get; set; }                             //权值
+        public decimal value { get; set; }                              //计算数值
+        public int gain_or_damage { get; set; }                         //增益/损害属性
 
         public ModelNode()
         {
@@ -29,13 +30,14 @@ namespace NET_Framwork48.Models
             prelevel = null;
             weight = 0;
             value = 0;
+            gain_or_damage = ModelWeight.NODE_AFFECT_GAIN + ModelWeight.NODE_AFFECT_DAMAGE;
         }
 
         public ModelNode(string nodename, string nodename_detail, int nodelevel, string nodelevelname)
         {
             NodeName = nodename;
             NodeName_Detail = nodename_detail;
-            NodeLevel = NodeLevel;
+            NodeLevel = nodelevel;
             NodeLevelName = nodelevelname;
         }
     }
--- Assessment Tool Prototype/NET Framwork48/Models/ModelWeight.cs
+++ Assessment Tool Prototype/NET Framwork48/Models/ModelWeight.cs
@@ -8,6 +8,82 @@ namespace NET_Framwork48.Models
 {
     class ModelWeight
     {
+        public const int NODE_AFFECT_GAIN = 1;
+        public const int NODE_AFFECT_DAMAGE = 2;
 
+        public void CreateModelNodeAffectDictionary(Dictionary<string,int> dic)
+        {
+            dic.Add(ModelName.LEVELNAME_QUALITYATTribute_1, NODE_AFFECT_GAIN);      //层1 Maintainability
+            dic.Add(ModelName.LEVELNAME_ATTRIBUTE_1, NODE_AFFECT_GAIN);             //层2 Analyzability
+            dic.Add(ModelName.LEVELNAME_ATTRIBUTE_2, NODE_AFFECT_GAIN);             //层2 Changeability
+            dic.Add(ModelName.LEVELNAME_ATTRIBUTE_3, NODE_AFFECT_GAIN);             //层2 Stability
+            dic.Add(ModelName.LEVELNAME_ATTRIBUTE_4, NODE_AFFECT_GAIN);             //层2 Modularity
+            dic.Add(ModelName.LEVELNAME_PROPERTY_1, NODE_AFFECT_DAMAGE);            //层3 Coupling
+            dic.Add(ModelName.LEVELNAME_PROPERTY_2, NODE_AFFECT_GAIN);              //层3 Cohesion
+            dic.Add(ModelName.LEVELNAME_PROPERTY_3, NODE_AFFECT_DAMAGE);            //层3 Design Complexity
+            dic.Add(ModelName.LEVELNAME_PROPERTY_4, NODE_AFFECT_DAMAGE);            //层3 System Size
+            dic.Add(ModelName.LEVELNAME_PROPERTY_5, NODE_AFFECT_GAIN);              //层3 Service Granularity
+            dic.Add(ModelName.LEVELNAME_PROPERTY_6, NODE_AFFECT_GAIN);              //层3 Parameter Granularity
+            dic.Add(ModelName.LEVELNAME_PROPERTY_7, NODE_AFFECT_DAMAGE);            //层3 Service Loopback
+            dic.Add(ModelName.LEVELNAME_METRIC_1, NODE_AFFECT_GAIN);                //层4 NS
+            dic.Add(ModelName.LEVELNAME_METRIC_2, NODE_AFFECT_GAIN);                //层4 NDCS()
+            dic.Add(ModelName.LEVELNAME_METRIC_3, NODE_AFFECT_GAIN);                //层4 NM
+            dic.Add(ModelName.LEVELNAME_METRIC_4, NODE_AFFECT_GAIN);                //层4 NIS()
+            dic.Add(ModelName.LEVELNAME_METRIC_5, NODE_AFFECT_GAIN);                //层4 NPI()
+            dic.Add(ModelName.LEVELNAME_METRIC_6, NODE_AFFECT_GAIN);                //层4 NII()
+            dic.Add(ModelName.LEVELNAME_METRIC_7, NODE_AFFECT_GAIN);                //层4 NMP()
+            dic.Add(ModelName.LEVELNAME_METRIC_8, NODE_AFFECT_GAIN);                //层4 WISL()
+        }
+
+        //写入节点的增益/损害属性
+        public void SetNodeGainORDamage(ModelNode node, Dictionary<string, int> dic)
+        {
+            int result;
+            dic.TryGetValue(node.NodeName, out result);
+            node.gain_or_damage = result;
+        }
+
+        //获取某一个节点下属的所有节点的增益/损害数
+        public void CollectNodeGainORDamageNum(ModelNode node, out decimal gain_num, out decimal damage_num)
+        {
+            gain_num = 0;
+            damage_num = 0;
+            foreach (ModelNode no in node.nextlevel)
+            {
+                if (no.gain_or_damage == NODE_AFFECT_GAIN)
+                    gain_num++;
+                else
+                    damage_num++;
+            }
+        }
+
+        //根据增益/损害数计算某一节点的权值
+        public void CalculateWeight(ModelNode node, int gain_num, int damage_num, out decimal weight)
+        {
+            decimal ten = gain_num + damage_num;
+            decimal gen = gain_num;
+            decimal den = damage_num;
+            weight = 0;
+            //该节点为增益节点，计算GEW（Gain Elements' Weight）
+            if (node.gain_or_damage == NODE_AFFECT_GAIN)
+            {
+                if (gain_num != 0 && damage_num == 0)
+                    weight = 1 / gen;
+                if (gain_num == 0 && damage_num != 0)
+                    weight = 0;
+                if (gain_num != 0 && damage_num != 0)
+                    weight = (ten + 1) / (ten * gen);
+            }
+            //该结点为损害节点，计算DEW（Damage Elements' Weight）
+            else
+            {
+                if (gain_num != 0 && damage_num == 0)
+                    weight = -0;
+                if (gain_num == 0 && damage_num != 0)
+                    weight = -1 / (2 * den);
+                if (gain_num != 0 && damage_num != 0)
+                    weight = -1 / (ten * den);
+            }
+        }
     }
 }
