59c4a1e04bb66028cb88c8e16292f01d499d871e
cyX_GUG <chenyangcyx@outlook.com>
2020-04-12 20:41:01 +0800

    更改了Model类的对象引用方式 时间： 2020年4月12日20:41:00

--- Assessment Tool Prototype/NET Framwork48/Models/Model.cs
+++ Assessment Tool Prototype/NET Framwork48/Models/Model.cs
@@ -13,13 +13,17 @@ namespace NET_Framwork48.Models
         public ModelNode root_node;
         //模型的各层节点列表
         public List<ModelNode> level1_nodes, level2_nodes, level3_nodes, level4_nodes;
-
+        //模型的必用操作项
+        OverAllData all = OverAllData.allData;
+        public ModelLink modelLink;
+        public ModelName modelName;
+        public ModelWeight modelWeight;
         //创建模型
         public void CreateModel()
         {
-            ModelLink ml = new ModelLink();
-            ModelName mn = new ModelName();
-            ModelWeight mw = new ModelWeight();
+            modelLink = new ModelLink();
+            modelName = new ModelName();
+            modelWeight = new ModelWeight();
             level1_nodes = new List<ModelNode>();
             level2_nodes = new List<ModelNode>();
             level3_nodes = new List<ModelNode>();
@@ -27,27 +31,27 @@ namespace NET_Framwork48.Models
             //创建第一层
             root_node = new ModelNode();     //第1层节点
             level1_nodes.Add(root_node);
-            mn.SetNodeName(root_node, ml.LEVEL1_UNITS);     //设置节点名称
-            mw.SetNodeGainORDamage(root_node);
+            modelName.SetNodeName(root_node, modelLink.LEVEL1_UNITS);     //设置节点名称
+            modelWeight.SetNodeGainORDamage(root_node);
             //创建第二层
-            for(int i=0;i<ml.LEVEL1_LINK_2_NUM;i++)
+            for(int i=0;i<modelLink.LEVEL1_LINK_2_NUM;i++)
             {
                 ModelNode node_temp2 = new ModelNode();     //第2层节点
                 level2_nodes.Add(node_temp2);
-                mn.SetNodeName(node_temp2, ml.LEVEL1_LINK_2_UNITS[i]);
-                mw.SetNodeGainORDamage(node_temp2);
-                for(int j = 0; j < ml.LEVEL2_LINK_3_NUM[i]; j++)
+                modelName.SetNodeName(node_temp2, modelLink.LEVEL1_LINK_2_UNITS[i]);
+                modelWeight.SetNodeGainORDamage(node_temp2);
+                for(int j = 0; j < modelLink.LEVEL2_LINK_3_NUM[i]; j++)
                 {
                     ModelNode node_temp3 = new ModelNode();     //第3层节点
                     level3_nodes.Add(node_temp3);
-                    mn.SetNodeName(node_temp3, ml.LEVEL2_LINK_3_UNITS[i][j]);
-                    mw.SetNodeGainORDamage(node_temp3);
-                    for(int k=0;k<ml.LEVEL3_LINK_4_NUM[i][j];k++)
+                    modelName.SetNodeName(node_temp3, modelLink.LEVEL2_LINK_3_UNITS[i][j]);
+                    modelWeight.SetNodeGainORDamage(node_temp3);
+                    for(int k=0;k<modelLink.LEVEL3_LINK_4_NUM[i][j];k++)
                     {
                         ModelNode node_temp4 = new ModelNode();     //第4层节点
                         level4_nodes.Add(node_temp4);
-                        mn.SetNodeName(node_temp4, ml.LEVEL3_LINK_4_UNITS[i][j][k]);
-                        mw.SetNodeGainORDamage(node_temp4);
+                        modelName.SetNodeName(node_temp4, modelLink.LEVEL3_LINK_4_UNITS[i][j][k]);
+                        modelWeight.SetNodeGainORDamage(node_temp4);
                         node_temp4.prelevel = node_temp3;
                         node_temp3.nextlevel.Add(node_temp4);
                     }
@@ -63,7 +67,6 @@ namespace NET_Framwork48.Models
         public void TrimModel()
         {
             //判断是否评估Modularity
-            OverAllData all = OverAllData.allData;
             //如果不评估
             if (!all.if_assess_modularity)
             {
@@ -105,19 +108,19 @@ namespace NET_Framwork48.Models
         //模型的权值计算
         public void CalculateModelWeight()
         {
-            ModelWeight mw = new ModelWeight();
+            modelWeight = new ModelWeight();
             //计算第一层的权值
             foreach (var node in level1_nodes)
-                mw.CalculateNodeNextLevelWeight(node);
+                modelWeight.CalculateNodeNextLevelWeight(node);
             //计算第二层的权值
             foreach (var node in level2_nodes)
-                mw.CalculateNodeNextLevelWeight(node);
+                modelWeight.CalculateNodeNextLevelWeight(node);
             //计算第三层的权值
             foreach (var node in level3_nodes)
-                mw.CalculateNodeNextLevelWeight(node);
+                modelWeight.CalculateNodeNextLevelWeight(node);
             //计算第四层的权值
             foreach (var node in level4_nodes)
-                mw.CalculateNodeNextLevelWeight(node);
+                modelWeight.CalculateNodeNextLevelWeight(node);
         }
     }
 }
