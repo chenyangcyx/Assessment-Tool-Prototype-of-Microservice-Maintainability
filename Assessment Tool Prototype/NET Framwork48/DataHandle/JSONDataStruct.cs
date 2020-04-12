7244d0107239f6c6bd6a086a3cff886767b26d89
cyX_GUG <chenyangcyx@outlook.com>
2020-04-12 17:17:04 +0800

    添加了JSONData的实体类 时间： 2020年4月12日17:17:02

---
+++ Assessment Tool Prototype/NET Framwork48/DataHandle/JSONDataStruct.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET_Framwork48.DataHandle.JSONDataStruct
+{
+    public class Version
+    {
+        public string sha1hash { get; set; }
+        public string tag { get; set; }
+    }
+
+    public class RepositoryInfoItem
+    {
+        public string repositoryURL { get; set; }
+        public string gitURL { get; set; }
+        public Version version { get; set; }
+    }
+
+    public class InterfacesItem
+    {
+        public string interfaceName { get; set; }
+        public List<string> messageName { get; set; }
+    }
+
+    public class CallItem
+    {
+        public string serviceName { get; set; }
+        public List<string> callInterface { get; set; }
+    }
+
+    public class MicroservicesItem
+    {
+        public string serviceName { get; set; }
+        public List<InterfacesItem> interfaces { get; set; }
+        public List<CallItem> call { get; set; }
+    }
+
+    public class MessagesItem
+    {
+        public string messageName { get; set; }
+        public string parameterNumber { get; set; }
+        public List<string> elements { get; set; }
+    }
+
+    public class Root
+    {
+        public string projectName { get; set; }
+        public List<RepositoryInfoItem> repositoryInfo { get; set; }
+        public List<MicroservicesItem> microservices { get; set; }
+        public List<MessagesItem> messages { get; set; }
+    }
+}
