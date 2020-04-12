baabfa06215e043e053050cfd270390953759df6
cyX_GUG <chenyangcyx@outlook.com>
2020-04-12 20:36:41 +0800

    添加了JSON数据的反序列化操作 时间： 2020年4月12日20:36:37

---
+++ Assessment Tool Prototype/NET Framwork48/DataHandle/DataInput.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET_Framwork48.DataHandle
+{
+    class DataInput
+    {
+        Encoding FILE_ENCODING = Encoding.UTF8;
+        public string InputDataFromFile(string path)
+        {
+            StreamReader sr = new StreamReader(path, FILE_ENCODING);
+            string content = sr.ReadToEnd();
+            sr.Close();
+            return content;
+        }
+    }
+}
